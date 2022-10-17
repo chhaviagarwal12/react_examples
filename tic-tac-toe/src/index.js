2f38eea3e68215f01f0aa46c311eb60057947ba2
chhaviagarwal12 <chhavi.1296@gmail.com>
2022-10-17 23:13:28 +0530

    rr

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom/client'
 import Game from './Components/App'
 const root=ReactDom.createRoot(document.getElementById('root'))
 root.render(
